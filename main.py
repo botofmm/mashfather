72859eea1521b4dd8d1bd6d57b0eb7fa8354a6b4
botofmm <107959556+botofmm@users.noreply.github.com>
2025-05-06 17:11:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from telegram.ext import Application, CommandHandler
 import os
 
-BOT_TOKEN = os.environ.get("8083903273:AAHSMjhrF7Hpi-9YSmi6ZwLoLQyspAkuorA")
+BOT_TOKEN = os.environ.get("6692402666:AAETpnGlaW2e5sezZLq6_WSbZ9Y5e1q4myA")
 
 async def start(update, context):
     await update.message.reply_text("Hello from Render! Your bot is running.")
